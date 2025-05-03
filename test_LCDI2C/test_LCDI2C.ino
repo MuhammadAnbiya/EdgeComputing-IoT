18970c7bd5a7536656c0992782571ab33c822c9c
MuhammadAnbiya <anbiya17agustus@gmail.com>
2025-05-03 18:53:20 +0700

    TANTEEEEE

--- test_LCDI2C/test_LCDI2C.ino
+++ test_LCDI2C/test_LCDI2C.ino
@@ -3,16 +3,49 @@
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); // alamat, kolom, baris
 
+// Lirik dan kecepatan (dalam milidetik per geseran)
+String lyrics[7] = {
+  "sudah terbiasa terjadi tanteee",
+  "teman datang ketika lagi butuh sajaaa",
+  "coba kalau lagi susah",
+  "mereka semua menghilaaaaaaaaaaaaaaaaaang",
+  "TANTEEEEEEEEEEEEEEEE",
+  "TANTEEEEEEEEEEEEEEEE",
+  "TANTEEEEEEEEEEEEEEEE",
+};
+
+int speeds[7] = {
+  67, // 0.2 detik per geser
+  60,
+  65,
+  67,
+  70,
+  70,
+  70
+};
+
 void setup() {
-  Wire.begin(18, 19); // Pin SDA, SCL khusus ESP32
-  lcd.begin(16, 2);   // Harus isi kolom & baris
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Hello ESP32!");
-  lcd.setCursor(0, 1);
-  lcd.print("LCD OK");
+  Wire.begin(18, 19);    // ESP32 SDA dan SCL
+  lcd.begin(16, 2);      // Inisialisasi LCD
+  lcd.backlight();       // Nyalakan backlight
+  lcd.clear();
 }
 
 void loop() {
-  lcd.print("temaku semua pada jahat tantee");
+  for (int i = 0; i < 7; i++) {
+    runText(lyrics[i], speeds[i]);
+    delay(150);         // jeda antar lirik
+    lcd.clear();
+  }
+}
+
+void runText(String text, int speed) {
+  text = "          " + text + "                  "; // padding spasi kanan-kiri
+
+  for (int i = 0; i <= text.length() - 16; i++) {
+    String segment = text.substring(i, i + 16);
+    lcd.setCursor(0, 0);              // baris atas
+    lcd.print(segment);               // tampilkan segmen
+    delay(speed);                     // kecepatan scroll
+  }
 }
