d2f91ac9ba86e0b8b6a373ed5cd7ac83a8adc3c8
MuhammadAnbiya <anbiya17agustus@gmail.com>
2025-05-04 20:13:14 +0700

    done +LDR

--- EdgeComputingV3/EdgeComputingV3.ino
+++ EdgeComputingV3/EdgeComputingV3.ino
@@ -1,68 +1,61 @@
 #include <Wire.h>
-//#include <BH1750.h>
 #include "DHT.h"
 
-
-#define DHTPIN 27         
-#define DHTTYPE DHT11 
+#define DHTPIN 27
+#define DHTTYPE DHT11
 #define BUZZER_PIN 14
 #define RELAY_PIN 12
+#define LDR_PIN 35
 
-
-//BH1750 lightMeter;
 DHT dht(DHTPIN, DHTTYPE);
 
-
-void setup(){
+void setup() {
   Serial.begin(9600);
   dht.begin();
-  pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, HIGH);
-//  Wire.begin(21, 22);
 
-//  if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)){
-//    Serial.println("Sensor Siap...");
-//  } else{
-//    Serial.println("Sensor Gagal");
-//    while(1);
-//  }
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(LDR_PIN, OUTPUT);
+
+  digitalWrite(RELAY_PIN, HIGH);
+  noTone(BUZZER_PIN);           
 }
 
 void loop() {
-  //  float lux = lightMeter.readLightLevel();
-  //  Serial.print("Intensitas Cahaya: ");
-  //  Serial.print(lux);
-  //  Serial.println(" Lux");
-
-//=== BUZZER ===
-  tone(BUZZER_PIN, 1000);
-  delay(1000);
-  noTone(BUZZER_PIN);
-  delay(1000);
-
-
-//=== DHT11 ===
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
   if (isnan(h) || isnan(t)) {
-    Serial.println("Gagal membaca dari sensor DHT22!");
-  } else {
-    Serial.print("Suhu: ");
-    Serial.print(t);
-    Serial.print("°C  |  Kelembapan: ");
-    Serial.print(h);
-    Serial.println("%");
+    Serial.println("Gagal membaca dari sensor DHT11!");
+    return;
   }
-  
+
+  Serial.print("Suhu: ");
+  Serial.print(t);
+  Serial.print("°C  |  Kelembapan: ");
+  Serial.print(h);
+  Serial.println("%");
+
+  if (t > 32) {
+    tone(BUZZER_PIN, 1000);
+    digitalWrite(RELAY_PIN, LOW);
+    Serial.println("PERINGATAN: Suhu tinggi! Buzzer & Kipas ON");
+  } else {
+    noTone(BUZZER_PIN);
+    digitalWrite(RELAY_PIN, HIGH);
+    Serial.println("Suhu normal. Buzzer & Kipas OFF");
+  }
+
+  int ldrValue = analogRead(LDR_PIN);
+  Serial.print("Nilai LDR (Cahaya): ");
+  Serial.println(ldrValue);
+
+  if (ldrValue < 1000) {
+    Serial.println("Keadaan: Gelap");
+  } else {
+    Serial.println("Keadaan: Terang");
+  }
+
+  Serial.println("------------------------");
   delay(2000);
-
-//=== Relay === 
-  Serial.println("Relay ON (CLOSE)");
-  digitalWrite(RELAY_PIN, LOW);
-  delay(3000);
-
-  Serial.println("Relay OFF (OPEN)");
-  digitalWrite(RELAY_PIN, HIGH);
-  delay(3000);
- }
+}
