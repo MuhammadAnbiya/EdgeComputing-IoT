0d3bcccb9dc7fc217ed09789babf18e68517d138
MuhammadAnbiya <anbiya17agustus@gmail.com>
2025-05-04 08:24:37 +0700

    done relay

--- EdgeComputingV3/EdgeComputingV3.ino
+++ EdgeComputingV3/EdgeComputingV3.ino
@@ -6,6 +6,7 @@
 #define DHTPIN 27         
 #define DHTTYPE DHT11 
 #define BUZZER_PIN 14
+#define RELAY_PIN 12
 
 
 //BH1750 lightMeter;
@@ -15,6 +16,8 @@ DHT dht(DHTPIN, DHTTYPE);
 void setup(){
   Serial.begin(9600);
   dht.begin();
+  pinMode(RELAY_PIN, OUTPUT);
+  digitalWrite(RELAY_PIN, HIGH);
 //  Wire.begin(21, 22);
 
 //  if (lightMeter.begin(BH1750::CONTINUOUS_HIGH_RES_MODE)){
@@ -31,11 +34,14 @@ void loop() {
   //  Serial.print(lux);
   //  Serial.println(" Lux");
 
+//=== BUZZER ===
   tone(BUZZER_PIN, 1000);
   delay(1000);
   noTone(BUZZER_PIN);
   delay(1000);
 
+
+//=== DHT11 ===
   float h = dht.readHumidity();
   float t = dht.readTemperature();
 
@@ -51,4 +57,12 @@ void loop() {
   
   delay(2000);
 
+//=== Relay === 
+  Serial.println("Relay ON (CLOSE)");
+  digitalWrite(RELAY_PIN, LOW);
+  delay(3000);
+
+  Serial.println("Relay OFF (OPEN)");
+  digitalWrite(RELAY_PIN, HIGH);
+  delay(3000);
  }
